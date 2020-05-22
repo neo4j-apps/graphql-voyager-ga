9e9a6b2e9d9886a42cab65070d82ff465a057aa0
ABK <akollegger@gmail.com>
2020-05-22 11:33:01 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Voyager} from 'graphql-voyager';
+import './voyager.css';
+
+const url = new URL(window.location.href);
+const relateApiEndpoint = url.searchParams.get('neo4jDesktopApiUrl');
+
+const relateApiClientId = url.searchParams.get('neo4jDesktopGraphAppClientId');
+
+const graphQLEndpoint = relateApiEndpoint || 'https://movies.grandstack.io/';
+
+function introspectionProvider(query:string) {
+  return fetch(graphQLEndpoint, {
+    method: 'post',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'ClientId': relateApiClientId || 'missing client id'
+    },
+    body: JSON.stringify({query: query}),
+  }).then(response => response.json());
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Voyager 
+      introspection={introspectionProvider} 
+      workerURI={process.env.PUBLIC_URL + '/voyager.worker.js'}
+      />
   );
 }
 
