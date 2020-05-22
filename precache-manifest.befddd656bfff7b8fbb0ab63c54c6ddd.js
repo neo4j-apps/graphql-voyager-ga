1dea1a1b0f16f182feb8396b2c1f1a28803ddf10
ABK <akollegger@gmail.com>
2020-05-22 11:37:56 +0200

    Updates

---
+++ precache-manifest.befddd656bfff7b8fbb0ab63c54c6ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a0287cb32b1f3348d81671ebd18e24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b9e540f00d5c2e713c1",
+    "url": "./static/css/main.40c4f134.chunk.css"
+  },
+  {
+    "revision": "5e4f3d020e05b10494df",
+    "url": "./static/js/2.b9b901eb.chunk.js"
+  },
+  {
+    "revision": "3bdb22e6e62d3d668ef7401b2ae8c3bf",
+    "url": "./static/js/2.b9b901eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9e540f00d5c2e713c1",
+    "url": "./static/js/main.e7fc2765.chunk.js"
+  },
+  {
+    "revision": "4667d4fa6f0d08a32525",
+    "url": "./static/js/runtime-main.2a76c15c.js"
+  }
+]);
